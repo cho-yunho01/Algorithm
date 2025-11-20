0601f0ebea84eeb38e265f0bcfff24b7047999b0
cho-yunho01 <04whdbsgh30@naver.com>
2025-11-20 17:35:12 +0900

    11-20

---
+++ Algorithm/src/main/java/Num2501.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Num2501 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new  StringTokenizer(br.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        for(int i =1;i<=a;i++){
+            if(a%i==0){
+                list.add(i);
+            }
+        }
+        if(list.size()<b){
+            System.out.println("0");
+        }
+        else{
+            System.out.println(list.get(b-1));
+        }
+
+
+    }
+}
