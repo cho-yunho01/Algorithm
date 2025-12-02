800a4fab09d848ca16bdfbed67bab86d239c38ea
cho-yunho01 <04whdbsgh30@naver.com>
2025-12-02 23:45:07 +0900

    12-02

---
+++ Algorithm/src/main/java/Num11047.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Num11047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int count = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (a[i] <= m) {
+                count = count + (m / a[i]);
+                m = m % a[i];
+            }
+        }
+
+        System.out.println(count);
+    }
+
+}
