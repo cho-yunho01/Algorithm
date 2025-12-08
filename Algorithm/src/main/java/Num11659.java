8578ce324c399df220b9aaed2ec873efd6b617f3
cho-yunho01 <04whdbsgh30@naver.com>
2025-12-08 23:38:35 +0900

    12-08

---
+++ Algorithm/src/main/java/Num11659.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Num11659 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        long[] S = new long[n + 1];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            S[i] = S[i - 1] + Integer.parseInt(st.nextToken());
+        }
+        for (int q = 0; q < m; q++) {
+            st = new StringTokenizer(br.readLine());
+
+            int i = Integer.parseInt(st.nextToken());
+
+            int j = Integer.parseInt(st.nextToken());
+            System.out.println(S[j] - S[i - 1]);
+        }
+    }
+}
