213de3d08f596779352e670b7b69c2b242fb9df3
cho-yunho01 <04whdbsgh30@naver.com>
2026-01-13 20:11:57 +0900

    01-13

--- Algorithm/src/main/java/Num1966.java
+++ Algorithm/src/main/java/Num1966.java
@@ -17,21 +17,57 @@ public class Num1966 {
             st = new StringTokenizer(br.readLine());
             int m = Integer.parseInt(st.nextToken());
             int n = Integer.parseInt(st.nextToken());
-            int a[] = new int[m];
 
+            int result=0;
+            Deque<Document> deque = new ArrayDeque<>();
             st = new StringTokenizer(br.readLine());
 
-            for(int j=0;j<m;j++){
-                a[j] = Integer.parseInt(st.nextToken());
+
+            for(int t=0;t<m;t++){
+                deque.add(new Document(t,Integer.parseInt(st.nextToken())));
+                if(n==t){
+                    result = deque.peekLast().getPriority();
+                }
             }
             int count = 0;
-            Deque<Integer> deque = new ArrayDeque<>();
-            for(int j=0;j<m;j++){
-                deque.add(a[j]);
-            }
-            while(deque.getFirst()!=a[n]){
-                deque.add(deque.removeFirst());
-                count++;
+
+            while(deque.size()!=result){
+                int max = 0;
+                int max_index = 0;
+                for(Document d:deque){
+                    if(d.priority>max){
+                        max = d.priority;
+                        max_index = d.index;
+                    }
+                }
+
+                int minus;
+                // 제일 큰 수가 뒤에 있을 때
+                if(max_index>deque.size()/2){
+                    minus = deque.size()-max_index;
+                    for(int t=0;t<minus;t++){
+                        deque.add(deque.removeFirst());
+                        count++;
+                    }
+                }
+
+                // 제일 큰 수가 앞에 있을 때
+                else if(max_index<deque.size()/2){
+                    minus = deque.size()-max_index;
+                    for(int t=0;t<minus;t++){
+                        deque.add(deque.removeLast());
+                        count++;
+                    }
+                }
+                // 중앙에 있을 때
+                else{
+                    minus = deque.size()-1;
+                    for(int t=0;t<minus;t++){
+                        deque.add(deque.removeFirst());
+                        count++;
+                    }
+                }
+
             }
             sb.append(count).append("\n");
         }
@@ -46,6 +82,9 @@ public class Num1966 {
         Document(int priority, int index) {
             this.priority = priority;
             this.index = index;
+        }
+        public int getPriority() {
+            return priority;
         }
     }
 
