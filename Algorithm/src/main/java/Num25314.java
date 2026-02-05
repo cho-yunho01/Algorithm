9222017146e922c90fc97fee646226dfe01f7db6
cho-yunho01 <04whdbsgh30@naver.com>
2026-02-05 23:18:55 +0900

    02-05

---
+++ Algorithm/src/main/java/Num25314.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Num25314 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int count = n / 4;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < count; i++) {
+            sb.append("long ");
+        }
+        sb.append("int");
+
+        System.out.print(sb.toString());
+    }
+}
