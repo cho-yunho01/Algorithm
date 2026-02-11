61a47fd7f7ee61db75e960ab83cbca31616a76c9
cho-yunho01 <04whdbsgh30@naver.com>
2026-02-11 23:25:38 +0900

    02-11

---
+++ Algorithm/src/main/java/Num10171.java
@@ -0,0 +1,10 @@
+public class Num10171 {
+    public static void main(String[] args) {
+        System.out.println("""
+                \\    /\\
+                 )  ( ')
+                (  /  )
+                 \\(__)|
+                """);
+    }
+}
