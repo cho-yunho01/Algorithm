0cfcafb6be4182616fc6bce5d1c0a6edba199079
cho-yunho01 <04whdbsgh30@naver.com>
2026-02-14 23:04:16 +0900

    02-14

---
+++ Algorithm/src/main/java/Num33515.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Num33515 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T1 = sc.nextInt();
+        int T2 = sc.nextInt();
+
+        System.out.println(Math.min(T1, T2));
+
+    }
+}
