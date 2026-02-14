8792fed4735476583d8c3e0b3f63bcc5264966f8
cho-yunho01 <04whdbsgh30@naver.com>
2026-02-14 13:24:25 +0900

    02-14

---
+++ Algorithm/src/main/java/Num15963.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Num15963 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+
+        if (n == m) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+    }
+}
