a001c076fe9c6575cf72f9c42532d367887ef49e
cho-yunho01 <04whdbsgh30@naver.com>
2026-03-01 23:56:52 +0900

    03-01

---
+++ Algorithm/src/main/java/Num5522.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Num5522 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int a = sc.nextInt();
+            sum = sum + a;
+        }
+
+        System.out.println(sum);
+
+    }
+}
