70d58f00c5298d098845362fa220d4e58f0d127b
cho-yunho01 <04whdbsgh30@naver.com>
2026-03-08 21:27:40 +0900

    03-08

---
+++ Algorithm/src/main/java/Num10987.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Num10987 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        int count = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+    }
+}
