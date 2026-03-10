6bfc31f913f5baf4e79e7f2e9f492b32634b21fe
cho-yunho01 <04whdbsgh30@naver.com>
2026-03-10 23:26:21 +0900

    03-10

---
+++ Algorithm/src/main/java/Num35277.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Num35277 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int a = 2000;
+
+        System.out.println(n / a);
+    }
+}
