0ff76c25be672c50b35e6c2e6d1bf9f5f832e55b
cho-yunho01 <04whdbsgh30@naver.com>
2026-03-15 21:57:24 +0900

    03-15

---
+++ Algorithm/src/main/java/Num5337.java
@@ -0,0 +1,9 @@
+public class Num5337 {
+    public static void main(String[] args) {
+        System.out.println("""
+                .  .   .
+                |  | _ | _. _ ._ _  _
+                |/\\|(/.|(_.(_)[ | )(/.
+                """);
+    }
+}
